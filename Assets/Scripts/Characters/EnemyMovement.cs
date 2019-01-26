51d0060a3390874c8033650cf8c95a66450a9fc8
hiwoods <caolam50@gmail.com>
2019-01-26 02:16:22 -0700

    play around with collision

---
+++ Assets/Scripts/Characters/EnemyMovement.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log("hitting player");
+    }
+}
