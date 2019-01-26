eb0cde7fb0a80b74621cf183c632aa75fb818602
hiwoods <caolam50@gmail.com>
2019-01-26 09:51:23 -0700

    offical character

---
+++ Assets/Scripts/Characters/CharacterInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour
+{
+    public float speed;
+    public float turningSpeed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+
+        if (move != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(move);
+        }
+        rb.velocity = move * speed * Time.deltaTime;
+    }
+
+}
---
+++ Assets/Scripts/Characters/EnemyAI.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float speed = 50;
+
+    private Rigidbody rb;
+
+    private Transform chaseTarget;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (chaseTarget)
+        {
+            var direction = (chaseTarget.position - transform.position).normalized;
+            transform.rotation = Quaternion.LookRotation(direction);
+            rb.velocity = direction * speed * Time.deltaTime;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("chasing player");
+
+            chaseTarget = other.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("lost player");
+            chaseTarget = null;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Kill the player");
+        }
+    }
+}
