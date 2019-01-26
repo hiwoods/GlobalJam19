0e5c38ef7df4139e1888177d6759c40c3b8312b6
hiwoods <caolam50@gmail.com>
2019-01-26 01:02:13 -0700

    character movement

---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10;
+    private CharacterController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+    
+    void FixedUpdate()
+    {
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _controller.Move(move * Time.deltaTime * speed);
+    }
+}
