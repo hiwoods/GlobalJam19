b4b83b223be048bb7587b89eac278fcf22f403a3
eneso <e.furkanoguz@gmail.com>
2019-01-27 15:01:44 -0700

    Music

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    AudioSource audioData;
+    public AudioClip clip;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioData = GetComponent<AudioSource>();
+               
+
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("somebody is in house");
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player enter house");
+
+            audioData.Play();
+
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("player left");
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player left house");
+            audioData.Stop();
+
+        }
+
+    }
+}
